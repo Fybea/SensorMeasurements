f6903dc7f200ac19fb49d642c5db161636c06769
Fybea <alexzey.198@gmail.com>
2024-07-17 16:27:00 +0300

    add annotations @Getter, @Setter, @NoArgsConstructor instead of default setters, getters and constructor removed annotation @NotNull from temperature field

--- src/main/java/com/rest/project/SpringRestProject/dto/SensorDTO.java
+++ src/main/java/com/rest/project/SpringRestProject/dto/SensorDTO.java
@@ -2,21 +2,16 @@ package com.rest.project.SpringRestProject.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class SensorDTO {
 
     @NotEmpty(message = " should not be empty")
     @Size(min = 2, max = 50, message = " should be between 2 and 50 characters")
     private String name;
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
 }
